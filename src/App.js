d16c1113672d00c332483345698e9ecda3201e12
MINTCHOC <85886984+Akatsuki812@users.noreply.github.com>
2025-12-25 11:44:58 +0700

    Add Comprnent

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header } from './func-components';
 import { Content } from './func-components'; 
 import { Footer } from './func-components';
-import { Calendar } from'./class-components';
+import Calendar from'./class-components';
 function App() {
   return (
   <> 
